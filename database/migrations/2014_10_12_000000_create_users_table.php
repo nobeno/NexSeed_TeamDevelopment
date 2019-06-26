ed758bdd0585b6a3e919ded3e3ecb1cabb9dad28
Takuto Ishida <>
2019-06-26 23:55:02 +0800

    開発準備のための、DBへの接続処理、AppServiceProviderの編集、MigrationFilesの作成と編集
    今回のDB名は「Kumamushi」とした。各自、env.ファイルを参照すること。
    なお、正しくMigrateするためのApp/Providers/AppServiceProvider.phpにおけるAppServiceProviderは編集済。編集箇所は「//追加」と記述してある。
    この度作成したMigrationFilesは、設計段階で作成したER図に記載の通りである。

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -15,10 +15,12 @@ class CreateUsersTable extends Migration
     {
         Schema::create('users', function (Blueprint $table) {
             $table->increments('id');
-            $table->string('name');
-            $table->string('email')->unique();
+            $table->string('nickname',30);
+            $table->string('email',50)->unique();
+            $table->string('password',30);
+            $table->string('user_img',255);
+            $table->string('profile',255);
             $table->timestamp('email_verified_at')->nullable();
-            $table->string('password');
             $table->rememberToken();
             $table->timestamps();
         });
